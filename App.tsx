61d381856d171ef6c8b299ae015753312d1d8072
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-30 23:44:57 +0100

    Config custom font

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
+/* eslint-disable react/style-prop-object */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {
+  useFonts,
+  Karla_400Regular as Karla400Regular,
+  Karla_700Bold as Karla700Bold,
+} from '@expo-google-fonts/karla';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Karla400Regular, Karla700Bold });
   return (
-    <View style={styles.container}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" backgroundColor="transparent" translucent />
+
+      {fontsLoaded ? (
+        <Text>Open up App.tsx to start working on your app!</Text>
+      ) : (
+        <View />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
