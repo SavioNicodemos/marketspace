59a248516cfbc80c4f82c60a0d358015ac8d02e7
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-04-01 22:09:41 +0100

    Create My Active ads banner

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@components/Button';
 import { UserPhoto } from '@components/UserPhoto';
-import { Box, HStack, Heading, Text, VStack } from 'native-base';
+import { Box, HStack, Heading, Text, VStack, Icon } from 'native-base';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Feather } from '@expo/vector-icons';
 
 export function Home() {
   return (
     <VStack>
-      <HStack mt={16} px={6} justifyContent="center">
+      <HStack mt={16} mx={6} justifyContent="center">
         <UserPhoto
           size={12}
           mr={2}
@@ -19,6 +21,35 @@ export function Home() {
           <Button title="Criar anúncio" icon="plus" />
         </Box>
       </HStack>
+
+      <VStack mx="6" mt="8">
+        <Text color="gray.300" fontSize="sm" mb="3">
+          Seus produtos anunciados para venda
+        </Text>
+        <HStack
+          bgColor="blue.50"
+          px="4"
+          py="3"
+          alignItems="center"
+          rounded="lg"
+        >
+          <Icon as={Feather} name="tag" color="blue.700" size="lg" />
+          <Box flex={1} ml={3}>
+            <Heading fontSize="lg" color="gray.200">
+              4
+            </Heading>
+            <Text color="gray.200" fontSize="xs">
+              anúncios ativos
+            </Text>
+          </Box>
+          <Box flexDir="row" alignItems="center">
+            <Heading color="blue.700" fontSize="xs">
+              Meus anúncios
+            </Heading>
+            <Icon as={Feather} name="arrow-right" color="blue.700" size="sm" />
+          </Box>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
