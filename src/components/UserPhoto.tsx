c991a58af6efa06033439b8053c76ed5cd0cb386
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-04-04 01:06:20 +0100

    Style UserPhoto

--- src/components/UserPhoto.tsx
+++ src/components/UserPhoto.tsx
@@ -1,17 +1,25 @@
 import { Image, IImageProps } from 'native-base';
+import { ColorType } from 'native-base/lib/typescript/components/types';
 
 type Props = IImageProps & {
   size: number;
+  borderWidth?: number;
+  borderColor?: ColorType;
 };
 
-export function UserPhoto({ size, ...rest }: Props) {
+export function UserPhoto({
+  size,
+  borderWidth = 3,
+  borderColor = 'blue.500',
+  ...rest
+}: Props) {
   return (
     <Image
       w={size}
       h={size}
       rounded="full"
-      borderWidth={3}
-      borderColor="blue.500"
+      borderWidth={borderWidth}
+      borderColor={borderColor}
       alt="Foto de perfil do usuário"
       {...rest}
     />
