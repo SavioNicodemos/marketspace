0b61e2f71e1e2b1472d122e168e86fb8e39b2da1
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-04-07 15:41:22 -0300

    Create FilterChip component

---
+++ src/components/FilterChip.tsx
@@ -0,0 +1,41 @@
+import { HStack, Text, Icon, Pressable } from 'native-base';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Octicons } from '@expo/vector-icons';
+import { useState } from 'react';
+
+type Props = {
+  title: string;
+};
+
+export function FilterChip({ title }: Props) {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <Pressable onPress={() => setIsActive(!isActive)}>
+      <HStack
+        pl="4"
+        pr={isActive ? 0 : 4}
+        py="1.5"
+        bg={isActive ? 'blue.500' : 'gray.500'}
+        rounded="full"
+        alignItems="center"
+      >
+        <Text
+          fontSize="xs"
+          fontWeight="bold"
+          color={isActive ? 'white' : 'gray.200'}
+        >
+          {title.toUpperCase()}
+        </Text>
+        {isActive ? (
+          <Icon
+            as={Octicons}
+            name="x-circle-fill"
+            color="white"
+            size="xs"
+            mx="1.5"
+          />
+        ) : null}
+      </HStack>
+    </Pressable>
+  );
+}
